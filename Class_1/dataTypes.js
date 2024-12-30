1099e38542f8bacf3e32be33ca3281068d236142
Surender Dabur <47842592+SurenderDabur@users.noreply.github.com>
2024-12-30 06:40:40 +0000

    container config done

---
+++ Class_1/dataTypes.js
@@ -0,0 +1,16 @@
+"use strict"; // to treat all js code as newer version
+
+//alert("Hello") is not allowed in nodejs, only on browser
+
+// number => range upto 2 to power 53
+// bigint => for larger number
+// string => ""
+// boolean => true/false 
+// null => it is a stand alone value but type of this is object
+// undefined 
+// symbol => unique
+// object 
+
+console.log(typeof undefined);
+console.log(typeof null);
+
