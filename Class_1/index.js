75fc51c71dce32a96a832506483867965e40333a
Surender Dabur <47842592+SurenderDabur@users.noreply.github.com>
2024-12-30 06:20:46 +0000

    container config done

--- Class_1/index.js
+++ Class_1/index.js
@@ -1,1 +1,20 @@
 console.log("Hello World!")
+
+
+const rollNumber = 9
+let Name = "Surender Dabur"
+var email = "skd@gmail.com" // Prefer not to use var because of issue in block scope and functional scope
+subject = "CSE"
+let section;
+
+console.log(Name, rollNumber, email, subject, section)
+
+// rollNumber = 5 not allowed
+Name = "SK Dabur"
+email = "skdabur@gmail.com"
+subject = "IT"
+section = "A"
+
+
+console.log(Name)
+console.log(Name, rollNumber, email, subject, section)
