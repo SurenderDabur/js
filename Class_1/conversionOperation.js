7a98ca056e0c7dec5ef9b0c0f1885ea6ed98e117
Surender Dabur <47842592+SurenderDabur@users.noreply.github.com>
2024-12-30 07:58:13 +0000

    conversion

---
+++ Class_1/conversionOperation.js
@@ -0,0 +1,22 @@
+let score = "23abc"
+//console.log(typeof score)
+
+let newScore = Number(score)
+//console.log(typeof(newScore))
+//console.log(newScore);
+
+let data = ""
+//console.log(typeof data);
+
+let newData = Boolean(data)
+//console.log(typeof newData);
+//console.log(newData);
+
+let title = "Hacking"
+//console.log(typeof title);
+//console.log(title);
+
+let newTitle = Boolean(title)
+//console.log(typeof newTitle)
+//console.log(newTitle)
+
