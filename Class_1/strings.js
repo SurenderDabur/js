eab523dd9c55b9df471b271fac47eb1d3cb73b94
Surender Dabur <47842592+SurenderDabur@users.noreply.github.com>
2025-01-09 06:40:48 +0000

    Stings in JS

---
+++ Class_1/strings.js
@@ -0,0 +1,7 @@
+const firstName = "Surender";
+const lastName = "Dabur";
+const DOB = "23/11/1993";
+
+console.log("My name is "+firstName + " " + lastName + " and my date of birth is " + DOB);
+
+console.log(`My name is ${firstName} ${lastName} and my DOB is ${DOB}`);
